d8c52ba8705b246139618b3d090cf5e887cd816d
Niilo Keinänen <niilo.keinanen@arction.com>
2021-11-17 14:23:33 +0200

    dvxcharts

---
+++ bench-new/bench_dvxchart.js
@@ -0,0 +1,121 @@
+const BENCHMARK_IMPLEMENTATION = (() => {
+
+  const beforeStart = () => {
+    return new Promise(async (resolve, reject) => {
+      const stylesheets = [
+        "lib/dvxCharts.chart.min.css",
+        "lib/dvxCharts.chart.min.css"
+      ]
+      for (const stylesheet of stylesheets) {
+        const domStylesheet = document.createElement('link')
+        domStylesheet.rel = 'stylesheet'
+        domStylesheet.type = 'text/css'
+        domStylesheet.href = stylesheet
+        document.body.append(domStylesheet)
+        await new Promise((resolve, reject) => {
+          domStylesheet.onload = resolve
+        })
+      }
+      const scripts = [
+        "lib/dvxCharts.chart.min.js",
+      ]
+      for (const script of scripts) {
+        const libScript = document.createElement('script')
+        libScript.src = script
+        document.body.append(libScript)
+        await new Promise((resolve, reject) => {
+          libScript.onload = resolve
+        })
+      }
+      if (! ('dvxCharts' in window)) {
+        // Library has to be manually downloaded and put into `lib` folder.
+        alert(`Library not found, refer to README Required dependencies on required files for this library.`)
+      }
+      resolve()
+    });
+  };
+
+  let totalDataPoints = 0
+  let existingDataPoints = 0
+  let data
+  let chart
+  let state
+  
+  const loadChart = (initialData) => {
+    return new Promise((resolve, reject) => {
+      totalDataPoints = initialData[0].length
+      existingDataPoints = totalDataPoints
+      data = initialData
+      
+      state = {
+        axes: [
+          {
+            type: 'linear',
+            location: 'bottom',
+            zoomEnabled: true,
+          },
+        ],
+        series: new Array(BENCHMARK_CONFIG.channelsCount)
+          .fill(0)
+          .map((_, iChannel) => ({
+            title: `Channel #${iChannel + 1}`,
+            type: 'line',
+            data: data[iChannel],
+            markers: null,
+            class: 'mySeries',
+          })),
+      };
+      chart = new dvxCharts.Chart(state);
+      chart.write('chart');
+
+      if (!BENCHMARK_CONFIG.ticksEnabled) {
+        // TODO IMMEDIATE: How to hide ticks ?
+      }
+
+      requestAnimationFrame(resolve)
+    });
+  };
+
+  const appendData = (newData) => {
+    return new Promise((resolve, reject) => {
+      newDataPointsCount = newData[0].length
+      totalDataPoints += newDataPointsCount
+      existingDataPoints += newDataPointsCount
+
+      const keepDataPointsCount = BENCHMARK_CONFIG.appendTimeDomainIntervalSeconds * BENCHMARK_CONFIG.appendNewSamplesPerSecond
+      const deleteDataPointsCount = Math.max((existingDataPoints) - keepDataPointsCount, 0)
+
+      for (let iCh = 0; iCh < BENCHMARK_CONFIG.channelsCount; iCh += 1) {
+        for (let i = 0; i < deleteDataPointsCount; i += 1) {
+          data[iCh].shift()
+        }
+        for (let i = 0; i < newDataPointsCount; i += 1) {
+          data[iCh].push(newData[iCh][i])
+        }
+      }
+      existingDataPoints -= deleteDataPointsCount
+      chart.setState(state);
+
+      requestAnimationFrame(resolve)
+    })
+  };
+
+  const refreshData = (data) => {
+    return new Promise((resolve, reject) => {
+      state.series.forEach((series, iCh) => {
+        series.data = data[iCh]
+      })
+      chart.setState(state)
+
+      requestAnimationFrame(resolve)
+    })
+  }
+
+  return {
+    dataFormat: 'xy-tuple-array',
+    beforeStart,
+    loadChart,
+    appendData,
+    refreshData,
+  };
+})();
--- bench-new/config.js
+++ bench-new/config.js
@@ -1,12 +1,12 @@
 const BENCHMARK_CONFIG = {
-  // TODO: dvxchart, echart, uplot
-  // "lcjs" | "scichart" | "plotly" | "amchart" | "anychart" | "highchart" | "zingchart"
-  library: "zingchart",
+  // TODO: echart, uplot
+  // "lcjs" | "scichart" | "plotly" | "amchart" | "anychart" | "highchart" | "zingchart" | "dvxchart" | "dygraph"
+  library: "dygraph",
   // "static" | "append" | "refresh"
   mode: "static",
   // In "static" or "refresh" mode, amount of data points per channel.
   channelDataPointsCount: 10000,
-  channelsCount: 1,
+  channelsCount: 10,
   ticksEnabled: false,
   dataSource: 'data.json',
   strokeThickness: 1,
@@ -15,9 +15,9 @@ const BENCHMARK_CONFIG = {
   // In "append" mode, interval of x axis as seconds.
   appendTimeDomainIntervalSeconds: 15,
   // In "append" mode, amount of samples pushed every second.
-  appendNewSamplesPerSecond: 1000,
+  appendNewSamplesPerSecond: 10000,
   // In "append" mode, the data that matches this many seconds is first appended into the chart to simulate as if the application had run for a long time.
   appendHistorySeconds: 15, 
   // In "refresh" mode, amount of refreshes every second.
-  refreshRate: 1,
+  refreshRate: 10,
 };
